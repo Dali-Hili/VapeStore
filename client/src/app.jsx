e450aef623d88df6e83cd6856e8ae9c75dd8813c
Semer-Ben-Salem <semer_ben_salem@yahoo.fr>
2021-02-26 15:36:07 +0100

    pulling nassir changes

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -25,8 +25,8 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-          <Product data={this.state.data} />
-          <Signup/>
+        <Product data={this.state.data} />
+        {/* <Signup/> */}
       </div>
     );
   }
