ff8ae1ea792afb0a44758c403130dc628bf7c1f0
Dali-Hili <you@example.com>
2021-03-01 16:00:50 +0100

    details

--- client/src/components/details.jsx
+++ client/src/components/details.jsx
@@ -1,27 +1,50 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-export default class product extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="product">
-        {this.props.data.map((prod, key) => {
-          return (
-            <div className="card" key={key}>
-              <div className="img">
-                <img src={prod.imageUrl} alt="" />
-              </div>
-              <div className="title">{prod.title}</div>
-              <div className="desc">{prod.description}</div>
-              <div className="btn">
-                <button>order</button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function Prodetail(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.product.imageUrl}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+         { props.product.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+           {props.product.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          {props.product.prise}
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
